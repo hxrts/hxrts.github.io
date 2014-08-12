fddf492edcf93d39034c495789614f7504961e4f
hxrts <sh@vvvnt.com>
2014-08-12 14:55:38 -0400

    adds nav highlighting

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,15 @@
 $('a').click(function(){
+
+    //$(this).parent.addClass('selected');
+
     $('html, body').animate({
         scrollTop: $( $(this).attr('href') ).offset().top
     }, 250);
+
+    //document.getElementByClass("selected").removeClass("selected");
+
+    $(".selected").removeClass("selected");
+    $(this).parent().addClass("selected");
+
     return false;
 });

73195c971a4152fd0f9b85d99de20e60e3034a1e
hxrts <sam@hxrts.com>
2015-11-08 18:28:29 -0500

    working on URL hash

--- js/scroll.js
+++ js/scroll.js
@@ -17,3 +17,17 @@ $('a').click(function(){
     return false;
     
 });
+
+$(document).bind('scroll',function(e){
+    $('div').each(function(){
+        if (
+           $(this).offset().top < window.pageYOffset + 10
+        //begins before top
+        && $(this).offset().top + $(this).height() > window.pageYOffset + 10
+        //but ends in visible area
+        //+ 10 allows you to change hash before it hits the top border
+        ) {
+            window.location.hash = $(this).attr('id');
+        }
+    });
+});

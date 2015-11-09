d9bab9586a7c93f9e0846b748dfdc119c933de53
hxrts <sam@hxrts.com>
2015-11-08 19:01:44 -0500

    working on URL hash

--- js/scroll.js
+++ js/scroll.js
@@ -18,16 +18,33 @@ $('a').click(function(){
     
 });
 
-$(document).bind('scroll',function(e){
-    $('div').each(function(){
-        if (
-           $(this).offset().top < window.pageYOffset + 10
-        //begins before top
-        && $(this).offset().top + $(this).height() > window.pageYOffset + 10
-        //but ends in visible area
-        //+ 10 allows you to change hash before it hits the top border
-        ) {
-            window.location.hash = $(this).attr('id');
-        }
+
+
+$(document).ready(function(){
+
+    function isScrolledIntoView(elem){
+        var $elem = $(elem);
+        var $window = $(window);
+
+        var docViewTop = $window.scrollTop();
+        var docViewBottom = docViewTop + $window.height();
+
+        var elemTop = $elem.offset().top;
+        var elemBottom = elemTop + $elem.height();
+
+        console.log($(elem));
+
+        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
+
+    $(window).scroll(function() {
+        if (isScrolledIntoView("#about")) { window.history.replaceState("state", "title", ""); return; }
+        if (isScrolledIntoView("#publications")) { window.history.replaceState("state", "title", "#publications"); return; }
+        if (isScrolledIntoView("#talks")) { window.history.replaceState("state", "title", "#talks"); return; }
+        if (isScrolledIntoView("#exhibitions")) { window.history.replaceState("state", "title", "#exhibitions"); return; }
+        if (isScrolledIntoView("#performances")) { window.history.replaceState("state", "title", "#performances"); return; }
+        if (isScrolledIntoView("#projects")) { window.history.replaceState("state", "title", "#projects"); return; }
+        if (isScrolledIntoView("#press")) { window.history.replaceState("state", "title", "#content"); return; }
     });
+
 });

23509eaa17a8bf1ac261da01e9ff5fa215264a88
hxrts <sam@hxrts.com>
2015-11-10 18:23:10 -0500

    get url hash test

--- js/scroll.js
+++ js/scroll.js
@@ -63,25 +63,27 @@ $(document).ready(function(){
             if (!$("#sidemenu li:last-child").hasClass("selected")) {
                 $("#sidemenu li:nth-last-child(3)").removeClass("selected");
                 $("#sidemenu li:last-child").addClass("selected");
-                // if("#" + window.location.hash.substr(1)!=aArray[aArray.length - 1]) {
-                //     var running = false,
-                //     limit = 5,
-                //     current = 0;
-                //     if (running === true) {
-                //         running = false;
-                //         return false;
-                //     }
-                //     running = true;
-                //     var end = setInterval(function () {
-                //         if (current >= limit || running == false) {
-                //             running = false;
-                //             clearInterval(end);
-                //         }
-                //         window.history.replaceState("state", "title", theID);
-                //         current++;
-                //     }, 500);
-                // }
             }
+
+            if("#" + window.location.hash.substr(1)!=aArray[aArray.length - 1]) {
+                var running = false,
+                limit = 5,
+                current = 0;
+                if (running === true) {
+                    running = false;
+                    return false;
+                }
+                running = true;
+                var end = setInterval(function () {
+                    if (current >= limit || running == false) {
+                        running = false;
+                        clearInterval(end);
+                    }
+                    window.history.replaceState("state", "title", theID);
+                    current++;
+                }, 500);
+            }
+            
         }
     });
 });

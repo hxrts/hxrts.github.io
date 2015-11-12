d261f1f98e82a930ef5b3f1652914a1191972301
hxrts <sam@hxrts.com>
2015-11-12 14:40:12 -0500

    git commit date

--- js/scroll.js
+++ js/scroll.js
@@ -71,4 +71,18 @@ $(document).ready(function(){
 
         }
     });
+
+    $.ajax({
+        url: 'https://api.github.com/repos/hxrts/hxrts.github.io/commits',
+        type: 'GET',
+        dataType: 'json',
+        success: function(result) {
+            commits = $.each(result, function(index, element) {});
+            date = commits[0]["commit"]["committer"]["date"];
+            formatdate = moment(date).format('MM.DD.YY');
+            document.getElementById("txtbox").value = "updated " + formatdate;
+        },
+        error: function() { console.log("error"); },
+    });
+
 });
